795a919ad14fb5d645e2d9b0ea04dcb6270896aa
Samuel Amsallem <amsallem.samuel@gmail.com>
2018-01-16 18:22:23 +0100

    projet spec

---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,9 @@
+#write your code here
+
+def ftoc(num)
+(num - 32)*5/9
+end
+
+def ctof(num)
+ (num * 1.8) + 32
+end
---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,17 @@
+#write your code here
+
+def add(num1,num2)
+    (num1 + num2)
+end 
+
+def subtract(num3,num4)
+	(num3 - num4)
+end
+
+
+def sum(num)
+	
+	while
+
+end
+		
