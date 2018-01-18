2a27a6c16ac50c9c0827db087499031ce809b933
SamuelAmsallem <35225199+SamuelAmsallem@users.noreply.github.com>
2018-01-18 18:30:35 +0100

    Update Nokogiri.rb

--- Nokogiri.rb
+++ Nokogiri.rb
@@ -1,12 +1,24 @@
 require 'nokogiri'
 require 'open-uri'
 
-# Fetch and parse HTML document
+#Je suis resté bloqué a la première consigne car je suis resté 
+#bloqué sur le premier scrapping, je voulais cibler le Style 22 
+#mais à chaque fois on m'affichait toute la page
+#Sachant que j'aime (un peu trop) trouver les solutions par moi même 
+# J'ai réussi à trouver sur le net que rajouter une commande type "tr td.style27" permettait de mieux cibler
+# Malheureusement je n'ai pas pus l'appliquer correctement
+
+#Au moins la correction sera rapide ;) 
+
 doc = Nokogiri::HTML(open('http://annuaire-des-mairies.com/95/vaureal.html').read)
 
-doc.search('.Style22').each do |link|
+doc.search('tr td.style27 p.Style22 font').each do |link|
   puts link.content
 end
+
+
+
+
 
 
 
